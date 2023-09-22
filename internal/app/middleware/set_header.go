8dac5fd86af3d91ba944929bb087cd05d02ba499
elij <elij@wundrd.net>
2023-09-21 17:36:52 -0700

    update

---
+++ internal/app/middleware/set_header.go
@@ -0,0 +1,14 @@
+package middleware
+
+import "net/http"
+
+// SetHeader is a convenience handler to set a response header key/value
+func SetHeader(key, value string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set(key, value)
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
