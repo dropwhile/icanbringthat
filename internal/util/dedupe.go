afde5c5835cb1f3c1f2dff15bee5fcc156f96120
elij <elij@wundrd.net>
2023-10-07 17:28:44 -0700

    small optimizations

--- internal/util/dedupe.go
+++ internal/util/dedupe.go
@@ -3,8 +3,8 @@ package util
 var empty = struct{}{}
 
 func RemoveDuplicates[T comparable](sliceList []T) []T {
-	allKeys := make(map[T]struct{})
-	list := []T{}
+	allKeys := make(map[T]struct{}, len(sliceList))
+	list := make([]T, 0, len(sliceList))
 	for _, item := range sliceList {
 		if _, value := allKeys[item]; !value {
 			allKeys[item] = empty
@@ -15,7 +15,7 @@ func RemoveDuplicates[T comparable](sliceList []T) []T {
 }
 
 func ToSet[T comparable](sliceList []T) map[T]struct{} {
-	keys := make(map[T]struct{})
+	keys := make(map[T]struct{}, len(sliceList))
 	for _, item := range sliceList {
 		keys[item] = empty
 	}
@@ -23,7 +23,7 @@ func ToSet[T comparable](sliceList []T) map[T]struct{} {
 }
 
 func ToSetIndexed[T comparable](sliceList []T) map[T]int {
-	keys := make(map[T]int)
+	keys := make(map[T]int, len(sliceList))
 	for i, item := range sliceList {
 		keys[item] = i
 	}
