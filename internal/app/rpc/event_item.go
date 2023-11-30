67de053ad7dc7d11991b1a553590eceebbfc2dac
elij <elij@wundrd.net>
2023-11-30 15:17:02 -0800

    update

--- internal/app/rpc/event_item.go
+++ internal/app/rpc/event_item.go
@@ -48,3 +48,49 @@ func (s *Server) ListEventItems(ctx context.Context,
 	}
 	return response, nil
 }
+
+func (s *Server) RemoveEventItem(ctx context.Context,
+	r *pb.RemoveEventItemRequest,
+) (*pb.RemoveEventItemResponse, error) {
+	// get user from auth in context
+	user, err := auth.UserFromContext(ctx)
+	if err != nil || user == nil {
+		return nil, twirp.Unauthenticated.Error("invalid credentials")
+	}
+
+	refID, err := model.ParseEventItemRefID(r.RefId)
+	if err != nil {
+		return nil, twirp.InvalidArgumentError("ref_id", "bad earmark ref-id")
+	}
+
+	eventItem, err := model.GetEventItemByRefID(ctx, s.Db, refID)
+	switch {
+	case errors.Is(err, pgx.ErrNoRows):
+		return nil, twirp.NotFoundError("event-item not found")
+	case err != nil:
+		return nil, twirp.InternalError("db error")
+	}
+
+	event, err := model.GetEventByID(ctx, s.Db, eventItem.EventID)
+	switch {
+	case errors.Is(err, pgx.ErrNoRows):
+		return nil, twirp.NotFoundError("event not found")
+	case err != nil:
+		return nil, twirp.InternalError("db error")
+	}
+
+	if event.UserID != user.ID {
+		return nil, twirp.PermissionDenied.Error("not event owner")
+	}
+
+	if event.Archived {
+		return nil, twirp.PermissionDenied.Error("event is archived")
+	}
+
+	err = model.DeleteEventItem(ctx, s.Db, eventItem.ID)
+	if err != nil {
+		return nil, twirp.InternalError("db error")
+	}
+
+	return &pb.RemoveEventItemResponse{}, nil
+}
