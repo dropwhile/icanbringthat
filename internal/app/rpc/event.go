736d28bf496a2a0689ae8e5a1dcfb3d126579b91
elij <elij@wundrd.net>
2023-11-30 21:40:06 -0800

    update

--- internal/app/rpc/event.go
+++ internal/app/rpc/event.go
@@ -3,6 +3,7 @@ package rpc
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/twitchtv/twirp"
@@ -70,6 +71,126 @@ func (s *Server) ListEvents(ctx context.Context,
 	response := &pb.ListEventsResponse{
 		Events:     dto.ToPbList(dto.ToPbEvent, events),
 		Pagination: paginationResult,
+	}
+	return response, nil
+}
+
+func (s *Server) CreateEvent(ctx context.Context,
+	r *pb.CreateEventRequest,
+) (*pb.CreateEventResponse, error) {
+	// get user from auth in context
+	user, err := auth.UserFromContext(ctx)
+	if err != nil || user == nil {
+		return nil, twirp.Unauthenticated.Error("invalid credentials")
+	}
+
+	name := r.Name
+	description := r.Description
+	when := r.When.Ts
+	tz := r.When.Tz
+
+	if name == "" {
+		return nil, twirp.RequiredArgumentError("name")
+	}
+	if description == "" {
+		return nil, twirp.RequiredArgumentError("description")
+	}
+	if when == nil {
+		return nil, twirp.RequiredArgumentError("when")
+	}
+	if tz == "" {
+		return nil, twirp.RequiredArgumentError("tz")
+	}
+
+	loc, err := time.LoadLocation(tz)
+	if err != nil {
+		return nil, twirp.InvalidArgument.Error("unrecognized tz")
+	}
+
+	startTime := when.AsTime()
+
+	event, err := model.NewEvent(ctx, s.Db,
+		user.ID, name, description, startTime, &model.TimeZone{Location: loc})
+	if err != nil {
+		return nil, twirp.InternalError("db error")
+	}
+
+	response := &pb.CreateEventResponse{
+		Event: dto.ToPbEvent(event),
+	}
+	return response, nil
+}
+
+func (s *Server) UpdateEvent(ctx context.Context,
+	r *pb.UpdateEventRequest,
+) (*pb.UpdateEventResponse, error) {
+	// get user from auth in context
+	user, err := auth.UserFromContext(ctx)
+	if err != nil || user == nil {
+		return nil, twirp.Unauthenticated.Error("invalid credentials")
+	}
+
+	refID, err := model.ParseEventRefID(r.RefId)
+	if err != nil {
+		return nil, twirp.InvalidArgumentError("ref_id", "bad notification ref-id")
+	}
+
+	event, err := model.GetEventByRefID(ctx, s.Db, refID)
+	switch {
+	case errors.Is(err, pgx.ErrNoRows):
+		return nil, twirp.NotFoundError("notification not found")
+	case err != nil:
+		return nil, twirp.InternalError("db error")
+	}
+
+	if user.ID != event.UserID {
+		return nil, twirp.PermissionDenied.Error("permission denied")
+	}
+
+	if r.Name == nil && r.Description == nil && r.When == nil {
+		return nil, twirp.InvalidArgument.Error("missing fields")
+	}
+
+	changes := false
+	if r.Name != nil && *r.Description != event.Description {
+		if *r.Name == "" {
+			return nil, twirp.RequiredArgumentError("name")
+		}
+		event.Name = *r.Name
+		changes = true
+	}
+	if r.Description != nil && *r.Description != event.Description {
+		if *r.Description == "" {
+			return nil, twirp.RequiredArgumentError("description")
+		}
+		event.Description = *r.Description
+		changes = true
+	}
+	if r.When != nil {
+		loc, err := time.LoadLocation(r.When.Tz)
+		if err != nil {
+			return nil, twirp.InvalidArgument.Error("unrecognized tz")
+		}
+
+		event.StartTime = r.When.Ts.AsTime()
+		event.StartTimeTz = &model.TimeZone{Location: loc}
+		changes = true
+	}
+
+	if !changes {
+		return nil, twirp.FailedPrecondition.Error("no changes")
+	}
+
+	if err := model.UpdateEvent(
+		ctx, s.Db, event.ID,
+		event.Name, event.Description, event.ItemSortOrder,
+		event.StartTime, event.StartTimeTz,
+	); err != nil {
+		return nil, twirp.InternalError("db error")
+	}
+
+	response := &pb.UpdateEventResponse{
+		Event: dto.ToPbEvent(event),
 	}
 	return response, nil
 }
