4bcc8e9953e62f114f5999ba20733450012e2563
elij <elij@wundrd.net>
2023-12-16 20:45:04 -0800

    add new file

---
+++ internal/app/rpc/event_test.go
@@ -0,0 +1,20 @@
+package rpc
+
+import (
+	"testing"
+)
+
+func TestServer_ListEvents(t *testing.T) {
+}
+
+func TestServer_GetEventDetails(t *testing.T) {
+}
+
+func TestServer_CreateEvent(t *testing.T) {
+}
+
+func TestServer_UpdateEvent(t *testing.T) {
+}
+
+func TestServer_DeleteEvent(t *testing.T) {
+}
