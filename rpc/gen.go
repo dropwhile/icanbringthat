dee95123597f59f0189f00cb0d098d6b467b4819
elij <elij@wundrd.net>
2024-05-19 21:06:47 -0700

    this is fine

--- rpc/gen.go
+++ rpc/gen.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-//go:build generate
-
 //go:generate protoc --proto_path=./protos --go_out=paths=source_relative:./icbt --twirp_out=paths=source_relative:./icbt ./protos/service.proto
 //go:generate protoc-go-inject-tag -input=./icbt/service.pb.go
 
