949ca6cf80cece478f5b18bef435c4e6d384175d
elij <elij@wundrd.net>
2024-07-01 07:42:07 -0700

    try f-tests
    ref: https://valyala.medium.com/f-tests-as-a-replacement-for-table-driven-tests-in-go-8814a8b19e9e

--- internal/htmx/hx_test.go
+++ internal/htmx/hx_test.go
@@ -18,211 +18,152 @@ func headerWith(key, value string) http.Header {
 func TestHxx_Boosted(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   bool
-	}{
-		{"test missing", http.Header{}, false},
-		{"present", headerWith("hx-boosted", "true"), true},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, expected bool) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.Boosted(); got != tt.want {
-				t.Errorf("Hxx.Boosted() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.Boosted(); got != expected {
+				t.Errorf("Hxx.Boosted() = %v, want %v", got, expected)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, false)
+	f("test presence", headerWith("hx-boosted", "true"), true)
 }
 
 func TestHxx_CurrentUrl_HasPrefix(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		prefix string
-		want   bool
-	}{
-		{"test missing", http.Header{}, "/something", false},
-		{
-			"present",
-			headerWith("hx-current-url", "http://example.com/something"),
-			"/something",
-			true,
-		},
-		{
-			"present but wrong",
-			headerWith("hx-current-url", "http://example.com/somethinX"),
-			"/something",
-			false,
-		},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, prefix string, want bool) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.CurrentUrl().HasPathPrefix(tt.prefix); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Hxx.CurrentUrl() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.CurrentUrl().HasPathPrefix(prefix); !reflect.DeepEqual(got, want) {
+				t.Errorf("Hxx.CurrentUrl() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, "/something", false)
+	f("present", headerWith("hx-current-url", "http://example.com/something"), "/something", true)
+	f("present but wrong", headerWith("hx-current-url", "http://example.com/somethinX"), "/something", false)
 }
 
 func TestHxx_HistoryRestoreRequest(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   bool
-	}{
-		{"test missing", http.Header{}, false},
-		{"present", headerWith("hx-history-restore-request", "true"), true},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, want bool) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.HistoryRestoreRequest(); got != tt.want {
-				t.Errorf("Hxx.HistoryRestoreRequest() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.HistoryRestoreRequest(); got != want {
+				t.Errorf("Hxx.HistoryRestoreRequest() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, false)
+	f("present", headerWith("hx-history-restore-request", "true"), true)
 }
 
 func TestHxx_Prompt(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   string
-	}{
-		{"test missing", http.Header{}, ""},
-		{"present", headerWith("hx-prompt", "hodor"), "hodor"},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, want string) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.Prompt(); got != tt.want {
-				t.Errorf("Hxx.Prompt() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.Prompt(); got != want {
+				t.Errorf("Hxx.Prompt() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, "")
+	f("present", headerWith("hx-prompt", "hodor"), "hodor")
 }
 
 func TestHxx_Request(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   bool
-	}{
-		{"test missing", http.Header{}, false},
-		{"present", headerWith("hx-request", "true"), true},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, want bool) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.IsRequest(); got != tt.want {
-				t.Errorf("Hxx.Request() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.IsRequest(); got != want {
+				t.Errorf("Hxx.Request() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, false)
+	f("present", headerWith("hx-request", "true"), true)
 }
 
 func TestHxx_Target(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   string
-	}{
-		{"test missing", http.Header{}, ""},
-		{"present", headerWith("hx-target", "#someid"), "#someid"},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, want string) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.Target(); got != tt.want {
-				t.Errorf("Hxx.Target() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.Target(); got != want {
+				t.Errorf("Hxx.Target() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, "")
+	f("present", headerWith("hx-target", "#someid"), "#someid")
 }
 
 func TestHxx_TriggerName(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   string
-	}{
-		{"test missing", http.Header{}, ""},
-		{"present", headerWith("hx-trigger-name", "someid"), "someid"},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, want string) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.TriggerName(); got != tt.want {
-				t.Errorf("Hxx.TriggerName() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.TriggerName(); got != want {
+				t.Errorf("Hxx.TriggerName() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, "")
+	f("present", headerWith("hx-trigger-name", "someid"), "someid")
 }
 
 func TestHxx_Trigger(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name   string
-		fields http.Header
-		want   string
-	}{
-		{"test missing", http.Header{}, ""},
-		{"present", headerWith("hx-trigger", "someid"), "someid"},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+	f := func(name string, headers http.Header, want string) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
 			t.Parallel()
-			hxx := &Hxr{
-				Header: tt.fields,
-			}
-			if got := hxx.Trigger(); got != tt.want {
-				t.Errorf("Hxx.Trigger() = %v, want %v", got, tt.want)
+			hxx := &Hxr{Header: headers}
+			if got := hxx.Trigger(); got != want {
+				t.Errorf("Hxx.Trigger() = %v, want %v", got, want)
 			}
 		})
 	}
+
+	f("test missing", http.Header{}, "")
+	f("present", headerWith("hx-trigger", "someid"), "someid")
 }
